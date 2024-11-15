c37e92c9d57e8886ed6b977fff34c31e5afcc388
mihaistoica97 <mihai.a.stoica@gmail.com>
2024-11-15 15:51:59 +0000

    Error handling on user input. I think I'm done with this, I could error handle more but I cba with a side project

--- sieve_of_eratosthenes/src/main.rs
+++ sieve_of_eratosthenes/src/main.rs
@@ -12,7 +12,7 @@ fn get_primes(max_number: u128) {
         if is_prime == true{
             print!(" {}", x)
         }
-    }
+    }    
 } 
 
 fn main() {
@@ -20,7 +20,7 @@ fn main() {
     let mut is_number = false;
     while is_number == false {        
         println!("GIVE ME A NUMBER");
-        std::io::stdin().read_line(&mut user_number).unwrap();
+        std::io::stdin().read_line(&mut user_number).unwrap_or(1);
         println!("Number is: {}", user_number);
         for x in user_number.chars(){
             if x == '\r' || x == '\n' {
@@ -32,8 +32,8 @@ fn main() {
             }
         }
     }
-    user_number.remove(user_number.len() - 1);
-    user_number.remove(user_number.len() - 1);
+    user_number = user_number.trim().to_owned();
+   
     let max_number = user_number.parse::<u128>().unwrap();
-    get_primes(max_number)
+    get_primes(max_number);
 }
